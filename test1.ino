c16d598e0fe4b6a633bb5340d4eb6870922f7f06
Jon Barnes <catotheelder@gmail.com>
2016-09-16 13:01:09 -0700

    push of test ino

---
+++ test1.ino
@@ -0,0 +1,31 @@
+/*
+*Code for Arduino Uno Board
+*Written in Atom
+*
+*First Test
+*
+*by, Jon Barnes
+*
+*Created: September 16 2016
+*Last Updated: September 16 2016
+*/
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
